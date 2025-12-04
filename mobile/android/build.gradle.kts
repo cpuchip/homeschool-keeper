e0c9e0c5df695b4fcd6465e1d1acd10bb84012c9
Michael Stufflebeam <cpuchip@gmail.com>
2025-12-03 18:18:12 -0600

    Fix Android Java deprecation warnings: force Java 17 for all subprojects

--- mobile/android/build.gradle.kts
+++ mobile/android/build.gradle.kts
@@ -5,6 +5,25 @@ allprojects {
     }
 }
 
+// Force all subprojects to use Java 17 to avoid deprecation warnings
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            extensions.findByType<com.android.build.gradle.BaseExtension>()?.apply {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+            }
+        }
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            compilerOptions {
+                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+            }
+        }
+    }
+}
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
